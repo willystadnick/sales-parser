fb93a6992a6b062783e3be0b3b16f482295e14d8
Willy Stadnick <willy.stadnick@gmail.com>
2020-07-22 19:36:08 -0300

    Add basic parser

--- parser.php
+++ parser.php
@@ -1,3 +1,13 @@
 <?php
 
-echo 'parser';
+$files = glob('data/in/*.dat');
+
+foreach ($files as $file) {
+	$info = pathinfo($file);
+	print('Parsing '.$info['basename'].'...'."\n");
+	$out = 'data/out/'.$info['filename'].'.done.'.$info['extension'];
+	$content = file_get_contents($file);
+	file_put_contents($out, $content);
+}
+
+print('Done.'."\n");
